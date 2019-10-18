b73721e8476eb210972bc865567e0dcfd366d6f1
Goldpm <lowedavidadam@gmail.com>
2019-10-18 18:40:07 -0400

    Created basic files and added html and css

---
+++ script.js
@@ -0,0 +1,6 @@
+// TODO: Get password button to create prompt
+// Get prompt that asks for (as well as validates): length of password, Confirm use of special characters, numbers
+// Create function that generates password according to specifications...
+// Have the generated password replace the p tag in the center of the box
+// Get Copy to Clipboard button working
+
