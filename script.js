d23dab395efd9968246eb45f221e84d7bdc21167
Goldpm <lowedavidadam@gmail.com>
2019-10-19 04:31:39 -0400

    added empty array

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ var arrayLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m
 var arrayNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var arraySpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
+var userPass = []
+
 function initParams() {
     userInput = parseInt(prompt("Enter desired password length (it must be 8-128 characters)"))
 
