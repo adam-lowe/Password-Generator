ecae8984b8537bc3f1fc4c8d0d8feca8ff5ec5a9
Goldpm <lowedavidadam@gmail.com>
2019-10-19 03:18:51 -0400

    added function that checks if input  is valid

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-// TODO: Get password button to create prompt
+// TODO: Get password button to create prompt <Check!>
 // Get prompt that asks for (as well as validates): length of password, Confirm use of special characters, numbers
 // Create function that generates password according to specifications...
 // Have the generated password replace the p tag in the center of the box
 // Get Copy to Clipboard button working
 
+function initParams() {
+ var userInput = parseInt(prompt("Enter desired password length (it must be 8-128 characters)"))
+
+if (isNaN(userInput)) {
+    alert("Error: Not a Number!")
+    return;
+ }
+else if (userInput > 128 || userInput < 8) {
+     alert("Error: Invalid Number of Characters!")
+     return;
+ }
+else {
+    firmParams();
+ }  
+}
