b467c168fb345c812ecc0ef780a9495f284c0bb5
Goldpm <lowedavidadam@gmail.com>
2019-10-19 04:21:02 -0400

    created arrays for pass specifications

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ var isLower
 var isNumber
 var isSpecial
 
+var arrayUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var arrayLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var arrayNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var arraySpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
 function initParams() {
     userInput = parseInt(prompt("Enter desired password length (it must be 8-128 characters)"))
 
@@ -34,7 +39,7 @@ function firmParams() {
     isSpecial = confirm("Do you want your password to have special characters?")
 
     if (isUpper || isLower || isNumber || isSpecial) {
-        console.log("Function will run Here!")
+        genPass();
 
     }
     else {
@@ -42,4 +47,8 @@ function firmParams() {
         return;
 
     }
+}
+
+function genPass() {
+    
 }
