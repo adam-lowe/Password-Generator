c6e14f1ed933b6bfec837401c6ac6a7a28aacae8
Goldpm <lowedavidadam@gmail.com>
2019-10-19 20:12:29 -0400

    Clipboard button now becomes active

--- script.js
+++ script.js
@@ -79,6 +79,7 @@ function genPass() {
 function showPass() {
     var userPass2 = userPass.join("")
     document.getElementById("passArea").textContent= userPass2
+    document.getElementById("clip").setAttribute("style", "background-color: #dc3545")
     userPass.length = 0
     userPass2.length = 0
 }
